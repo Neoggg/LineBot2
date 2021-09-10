74b8a6c3c04557c01e8e46fbd32f070d9b5d76e7
許家齊 <neogg@xujiaqis-MacBook-Pro.local>
2021-09-10 17:09:07 +0800

    inti

---
+++ basic/controllers/NeoController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use yii\web\Controller;
+
+class NeoController extends Controller
+{
+  public function actionIndex()
+  {
+    echo 'test';
+  }
+}
